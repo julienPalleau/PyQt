7bd65a1b830cd7af79ff77961b54f3e6b238f259
julienPalleau <jpalleau@gmail.com>
2023-10-18 13:28:38 +0200

    alignement

--- test.py
+++ test.py
@@ -1,198 +1,156 @@
-# https://stackabuse.com/working-with-pythons-pyqt-framework/
-# import sys
-# from PyQt6.QtWidgets import QApplication, QPushButton, QGridLayout, QWidget
-#
-# # Create the Qt Application
-# app = QApplication(sys.argv)
-#
-# # Create the parent widget
-# window = QWidget()
-#
-# # Create the buttons
-# button1 = QPushButton('One')
-# button2 = QPushButton('Two')
-# button3 = QPushButton('Three')
-# button4 = QPushButton('Four')
-# button5 = QPushButton('Five')
-#
-# # Create the QGrid Layout Manager
-# layout = QGridLayout(window)
-#
-# # Add button Widgets to the QGridLqyout
-# # addWidget([object], [row_number], [column number])
-# layout.addWidget(button1,0,0)
-# layout.addWidget(button2,1,0)
-# layout.addWidget(button3,2,0)
-# layout.addWidget(button4,0,1)
-# layout.addWidget(button5,0,2)
-#
-# # Add a title
-# window.setWindowTitle('A Simple PyQt6 App')
-# # Set a default box at x=100, y=100 and 280 wide and 80 height
-# window.setGeometry(100, 100, 280, 80)
-#
-# # Show the parent widget
-# window.show()
-#
-# # Run the main Qt loop
-# sys.exit(app.exec())
+list_essai = [{'experiment_procedure1': {'id': 1508, 'name': 'Description', 'section_type': 'description',
+                                         'collapsed': False, 'time_range': None, 'elements':
+                                             [{'id': 2442, 'uuid': '9cd64bc8-59de-4deb-9aa9-493f366a938e',
+                                               'element_type': 'text', 'rows': None, 'cols': None, 'name': None}],
+                                         'start_date': None, 'end_date': None,
+                                         'uuid': 'c2d0de73-8981-4bf6-9ded-07821d709654'}}, {
+                  'experiment_procedure2': {'id': 1509, 'name': 'Procedure', 'section_type': 'procedure',
+                                            'collapsed': False, 'time_range': None, 'elements':
+                                                [{'id': 2443, 'uuid': '62aae41e-bd9f-4dd5-954b-14bf59183319',
+                                                  'element_type': 'form', 'rows': None, 'cols': None, 'name': 'Form'}],
+                                            'start_date': None, 'end_date': None,
+                                            'uuid': '32f1b8d6-b21d-463f-888f-3ee2ee694fb2'}}, {
+                  'experiment_procedure3': {'id': 1510, 'name': 'Medium', 'section_type': 'custom', 'collapsed': False,
+                                            'time_range': None, 'elements':
+                                                [{'id': 2444, 'uuid': '5666e14e-15bf-49e9-895d-6c214e9eca19',
+                                                  'element_type': 'samples', 'rows': None, 'cols': None,
+                                                  'name': 'Samples & Reagents'}],
+                                            'start_date': None, 'end_date': None,
+                                            'uuid': '9e718e56-c420-43eb-9546-2efe6128611a'}}, {
+                  'experiment_procedure4': {'id': 1511, 'name': 'Results', 'section_type': 'results',
+                                            'collapsed': False,
+                                            'time_range': None, 'elements':
+                                                [{'id': 2445, 'uuid': '1eadcd2f-7d17-422c-8103-dc2f18922753',
+                                                  'element_type': 'text', 'rows': None, 'cols': None, 'name': None}],
+                                            'start_date': None, 'end_date': None,
+                                            'uuid': 'd101a38b-2940-4d82-93f2-15cd7e175c30'}}]
 
-# ------------------------------------------
+# print(f"longuer de la liste: {len(list_essai)}")
+# print(f"list_essai: {list_essai}\n")
 
-# # QFormLayout
-# import sys
-# from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QLineEdit
+# def trawler(list_essai):
+#     print(f'Debug 0 : {list_essai}')
+#     if isinstance(list_essai, str):
+#         print(f'Debug 1 : {list_essai}')
+#
+#     elif isinstance(list_essai, list):
+#         print("Debug 2 : list niveau 1")
+#         if len(list_essai) > 0:
+#             x = list_essai.pop(0)
+#             print(f"{x}\n")
+#             if isinstance(x, list):
+#                 print("Debug 3 : list niveau 3")
+#             elif isinstance(x, dict):
+#                 for key, value in x.items():
+#                     print(f'Debug 4 : key1---> {key}, value1---> {value}')
+#                     trawler(value)
+#             else:
+#                 print("Debug 5 : Error_1")
+#         elif isinstance(list_essai, dict):
+#             print("Debug 6 : list niveau 2")
+#             for key, value in list_essai.items():
+#                 print(f'Debug 7 : key2---> {key}, value2---> {value}')
+#                 trawler(list_essai)
+#     elif isinstance(list_essai, dict):
+#         print(f"Debug 8 : list_essai last {list_essai}")
+#         for key, value in list_essai.items():
+#             print(f'Debug 9 : key3---> {key}, value3---> {value}')
+#             trawler(value)
+#     elif isinstance(list_essai, str):
+#         print(f'Debug 10 : ERROR_2')
 #
 #
-# def addLabel(layout, text):
-#     layout.addWidget(QLabel(text))
-#
-#
-# # Create the Qt Application
-# app = QApplication(sys.argv)
-#
-# # Create the parent Widget and the QVBoxLayout Layout Manager
-# window = QWidget()
-# layout = QVBoxLayout(window)
-#
-# # Create a label Widget and add it to the Layout
-# label = QLabel('Enter some text!')
-# layout.addWidget(label)
-#
-# line_edit = QLineEdit()
-# layout.addWidget(line_edit)
-#
-# # Create a QPushButton object with a caption on it
-# qbtn = QPushButton('Add Label')
-#
-# # Add the QPushButton to the layout
-# layout.addWidget(qbtn)
-#
-# # Close the application whent the button is pressed
-# # Here I am using slots & signals, which I will demonstrate later in this tutorial.
-# qbtn.clicked.connect(lambda: addLabel(layout, line_edit.text()))
-#
-#
-# # Show the parent Widget
-# window.show()
-#
-# # Run the main Qt loop
-# sys.exit(app.exec())
+# trawler(list_essai)
 
-# ------------------------------------------
+list2_essai = {'experiment_procedure1': {'member_id':0, 'id': 1508, 'name': 'Description', 'section_type': 'description',
+                                         'collapsed': False, 'time_range': None,
+                                         'start_date': None, 'end_date': None,
+                                         'uuid': 'c2d0de73-8981-4bf6-9ded-07821d709654'},
 
-# Labels
+               'experiment_procedure2': {'id': 1509, 'name': 'Procedure', 'section_type': 'procedure',
+                                         'collapsed': False, 'time_range': None,
+                                         'start_date': None, 'end_date': None,
+                                         'uuid': '32f1b8d6-b21d-463f-888f-3ee2ee694fb2'},
 
-# import sys
-# from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
-# from PyQt6.QtCore import Qt
-#
-# # Create the Qt Application
-# app = QApplication(sys.argv)
-#
-# # Create the parent widget and the QVBoxLayout Layout Manager
-# window = QWidget()
-# layout = QVBoxLayout(window)
-#
-# # Create a Label beforehand
-# firstLabel = QLabel('Countrycode: BE')
-# secondLabel = QLabel('Brussels waffles are the best food ever.')
-#
-# # Add Labels to layout, creating an anonymous label while adding
-# layout.addWidget(firstLabel)
-# layout.addWidget(secondLabel, alignment=Qt.AlignmentFlag.AlignJustify)
-# layout.addWidget(QLabel('The Belgian flag consists of the colors black, yellow and red', wordWrap=True), alignment=Qt.AlignmentFlag.AlignLeft)
-#
-# # using setText() we can change the caption of a label
-# firstLabel.setText('Belgium is a country located in Europe')
-# firstLabel.setAlignment(Qt.AlignmentFlag.AlignRight)
-#
-# # Show the parent Widget
-# window.show()
-#
-# # Run the main Qt Loop
-# sys.exit(app.exec())
+               'experiment_procedure3': {'id': 1510, 'name': 'Medium', 'section_type': 'custom', 'collapsed': False,
+                                         'time_range': None,
+                                         'start_date': None, 'end_date': None,
+                                         'uuid': '9e718e56-c420-43eb-9546-2efe6128611a'},
+
+               'experiment_procedure4': {'id': 1511, 'name': 'Results', 'section_type': 'results',
+                                         'collapsed': False,
+                                         'time_range': None}
+               }
+
+students = {
+    'ID 1': {'Name': 'Shaun', 'Age': 35, 'City': 'Delhi'},
+    'ID 2': {'Name': 'Ritika', 'Age': 31, 'City': 'Mumbai'},
+    'ID 3': {'Name': 'Smriti', 'Age': 33, 'City': 'Sydney'},
+    'ID 4': {'Name': 'Jacob', 'Age': 23, 'City': {'perm': 'Tokyo',
+                                                  'current': 'London'}},
+}
+
+result = []
 
 
-# from PyQt6.QtCore import pyqtSlot
+# def nested_dict_pairs_iterator(dict_obj):
+#     ''' This function accepts a nested dictionary as argument
+#         and iterate over all values of nested dictionaries
+#     '''
+#     # Iterate over all key-value pairs of dict argument
+#     if isinstance(dict_obj, dict):
+#         for key, value in dict_obj.items():
+#             # Check if value is of dict type
+#             if isinstance(value, dict):
+#                 # If value is dict then iterate over all its values
+#                 for pair in nested_dict_pairs_iterator(value):
+#                     yield (key, *pair)
+#             else:
+#                 # If value is not dict type then yield the value
+#                 yield (key, value)
+#     elif isinstance(dict_obj, list):
+#         print("this is a list")
+#         for line in dict_obj:
+#             if isinstance(line, dict):
+#                 print("test1")
+#                 for key, value in line.items():
+#                     # Check if value is of dict type
+#                     print(f"key --> {key}, value --> {value}")
+#                     if isinstance(value, dict):
+#                         # If value is dict then iterate over all its values
+#                         for pair in nested_dict_pairs_iterator(value):
+#                             yield (key, *pair)
+#                     else:
+#                         # If value is not dict type then yield the value
+#                         yield (key, value)
 #
-# # Slot function - Note the @pyqtSlot() annotation!
-# @pyqtSlot()
-# def hello_world():
-#   print('Button is clicked, Hello World!')
-
-# ------------------------------------------
-
-# Signal and Slots
-
-# import sys
-# from PyQt6.QtWidgets import QApplication, QPushButton
-# from PyQt6.QtCore import pyqtSlot
 #
 #
-# @pyqtSlot()
-# def hello_world():
-#     print('You shall not pass')
 #
 #
-# # Create the Qt Application
-# app = QApplication(sys.argv)
-#
-# # Create a QPushButton Object
-# button = QPushButton('Click me')
-#
-# # Connect the button to the hello_world slot function
-# button.clicked.connect(hello_world)
-#
-# # Show the button to the user
-# button.show()
-#
-# # Run the main Qt loop
-# sys.exit(app.exec())
+# # Loop through all key-value pairs of a nested dictionary
+# for pair in nested_dict_pairs_iterator(list_essai):
+#     print(pair)
 
-# ------------------------------------------
+def get_id(value, keyword: str):
+    id_list = []
+    if isinstance(value, dict):
+        # get id if exist from dict
+        if id := value.get(keyword):
+            id_list += [id]
 
-# Buttons
-import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QLineEdit, QListWidget, QListWidgetItem
+        # get id's from nested list or dicts
+        for val in value.values():
+            if isinstance(val, list) or isinstance(val, dict):
+                id_list += get_id(val, keyword)
 
+    if isinstance(value, list):
+        for val in value:
+            if isinstance(val, list) or isinstance(val, dict):
+                id_list += get_id(val, keyword)
 
-def addLabel(layout, text):
-    layout.addWidget(QLabel(text))
+    return id_list
 
 
-# Create the Qt Application
-app = QApplication(sys.argv)
-
-# Create the parent widget and the QVBoxLayout Layout Manager
-window = QWidget()
-layout = QVBoxLayout(window)
-
-# Create a QlistWidget
-listWidget = QListWidget()
-for i in range(10):
-    item = QListWidgetItem("Item %i" % i)
-    listWidget.addItem(item)
-
-listWidget.show()
-
-# Create a QLabel Widget and add it to the Layout
-label = QLabel("Add Label")
-layout.addWidget(label)
-
-# Create a QlineEdit to collect user data
-line_edit = QLineEdit()
-layout.addWidget(line_edit)
-
-# Create a QPushButton object with a caption on it
-qbtn = QPushButton('Add Label')
-layout.addWidget(qbtn)
-
-# When clicked, perform a callable function - addLabel()
-qbtn.clicked.connect(lambda:addLabel(layout, line_edit.text()))
-
-# Show the parent widget
-window.show()
-
-# Run the main Qt loop
-sys.exit(app.exec())
+print(get_id(list_essai, "id"))
+print("done")
